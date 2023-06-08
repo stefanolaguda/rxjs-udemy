c5a5c84dc1571e69f55875aba292021b9a865e36
EarthDragon10 <lagudadev@gmail.com>
2023-06-08 18:25:01 +0200

    added teardown

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,24 @@ import { Observable } from "rxjs";
 const Observable$ = new Observable<string>((subscriber) => {
     console.log("Observable executed");
     subscriber.next("ciao");
+    subscriber.next("Stefano");
+    subscriber.error(new Error("Failure observable"));
+    setTimeout(() => {
+        subscriber.next("Laguda");
+    }, 2000);
+
+    // TEARDOWN
+    return () => {
+        console.log("Teardown");
+    };
 });
 
 console.log("Before subscribe");
+
 Observable$.subscribe({
     next: (value) => console.log(value),
     error: (err) => console.log(err),
+    complete: () => console.log("Observable Completed"),
 });
+
 console.log("After subscribe");
