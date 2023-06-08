de35d9331d2d24d42a624235cde269d3e2748589
EarthDragon10 <lagudadev@gmail.com>
2023-06-08 12:11:41 +0200

    launched two subscriptions fort test

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { Observable } from "rxjs";
 
 const Observable$ = new Observable<string>((subscriber) => {
     subscriber.next("Stefano");
-    setTimeout(() => subscriber.next("Laguda"), 3000);
-    setTimeout(() => subscriber.next("Steppy"), 5000);
+    setTimeout(() => subscriber.next("Laguda"), 2000);
+    setTimeout(() => subscriber.next("Steppy"), 4000);
 });
 
-const testUnsuscribe = Observable$.subscribe((result) => console.log(result));
+console.log("Subscription 1");
+Observable$.subscribe((result) => console.log(result));
 
-setTimeout(() => testUnsuscribe.unsubscribe(),4000);
+console.log("Subscription 2");
+setTimeout(() => Observable$.subscribe((result) => console.log(result)), 1000);
+
+// const testUnsuscribe = Observable$.subscribe((result) => console.log(result));
+
+// setTimeout(() => testUnsuscribe.unsubscribe(),4000);
