cbe05bd5976d88b026fd402bf338c2a5b12dea35
EarthDragon10 <lagudadev@gmail.com>
2023-06-08 14:53:03 +0200

    added obaerver with one value

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import { Observable } from "rxjs";
 
 const Observable$ = new Observable<string>((subscriber) => {
-    subscriber.next("Stefano");
-    setTimeout(() => subscriber.next("Laguda"), 2000);
-    setTimeout(() => subscriber.next("Steppy"), 4000);
+    console.log("Observable executed");
+    subscriber.next("ciao");
 });
 
-console.log("Subscription 1");
-Observable$.subscribe((result) => console.log(result));
-
-console.log("Subscription 2");
-setTimeout(() => Observable$.subscribe((result) => console.log(result)), 1000);
-
-// const testUnsuscribe = Observable$.subscribe((result) => console.log(result));
-
-// setTimeout(() => testUnsuscribe.unsubscribe(),4000);
+console.log("Before subscribe");
+Observable$.subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log(err),
+});
+console.log("After subscribe");
